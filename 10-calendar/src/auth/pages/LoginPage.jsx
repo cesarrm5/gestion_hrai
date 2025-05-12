7c213f668f9136ca7396bfc2141292f2b20eacfa
Cesar <cesarrm5@gmail.com>
2025-05-12 17:50:03 -0600

    Consumo de servicio

--- 10-calendar/src/auth/pages/LoginPage.jsx
+++ 10-calendar/src/auth/pages/LoginPage.jsx
@@ -1,17 +1,63 @@
+import {useEffect} from 'react';
+import Swal from 'sweetalert2';
+import { useForm, useAuthStore } from '../../hooks';
 import './LoginPage.css';
 
+const loginFormFields = {
+    loginEmail:     '',
+    loginPassword: '',
+}
+
+const registerFormFields = {
+    registerEmail:     '',
+    registerName:      '',
+    registerPassword:  '',
+    registerPassword2: '',
+}
+
 export const LoginPage = () => {
+
+    const {startLogin, errorMessage} = useAuthStore();
+    ///const {startRegister, errorMessage} = useRegisterStore();
+
+    const {loginEmail, loginPassword, onInputChange:onLoginInputChange} = useForm(loginFormFields);
+    const {registerEmail, registerName, registerPassword, registerPassword2, onInputChange:onRegisterInputChange} = useForm(registerFormFields);
+
+    const loginSubmit = (event)=>{
+        event.preventDefault();
+        startLogin({email: loginEmail, password: loginPassword});
+    }
+    
+    const registerSubmit = (event)=>{
+        event.preventDefault();
+        if (registerPassword !== registerPassword2){
+            Swal.fire('Error en registro', 'Conttraseñas no son iguales', 'error');
+            return
+        }
+        startRegister({email: registerEmail, name:registerName, password: registerPassword, password2:registerPassword2});
+        console.log({registerEmail, registerName, registerPassword, registerPassword2});
+    }
+
+    useEffect(() => {
+        if(errorMessage != undefined){
+            Swal.fire('Error en la autenticación', errorMessage,'error');
+        }
+    }, [errorMessage]);
+
     return (
         <div className="container login-container">
             <div className="row">
-                <div className="col-md-6 login-form-1">
+                <div className="col-md-12 login-form-1">
                     <h3>Ingreso</h3>
-                    <form>
+                    <form onSubmit={loginSubmit}>
                         <div className="form-group mb-2">
                             <input 
                                 type="text"
                                 className="form-control"
                                 placeholder="Correo"
+                                name='loginEmail'
+                                value={loginEmail}
+                                onChange={onLoginInputChange}
                             />
                         </div>
                         <div className="form-group mb-2">
@@ -19,6 +65,9 @@ export const LoginPage = () => {
                                 type="password"
                                 className="form-control"
                                 placeholder="Contraseña"
+                                name='loginPassword'
+                                value={loginPassword}
+                                onChange={onLoginInputChange}
                             />
                         </div>
                         <div className="d-grid gap-2">
@@ -31,14 +80,17 @@ export const LoginPage = () => {
                     </form>
                 </div>
 
-                <div className="col-md-6 login-form-2">
+                <div className="col-md-12 login-form-2">
                     <h3>Registro</h3>
-                    <form>
+                    <form onSubmit={registerSubmit}>
                         <div className="form-group mb-2">
                             <input
                                 type="text"
                                 className="form-control"
                                 placeholder="Nombre"
+                                name='registerName'
+                                value={registerName}
+                                onChange={onRegisterInputChange}
                             />
                         </div>
                         <div className="form-group mb-2">
@@ -46,6 +98,9 @@ export const LoginPage = () => {
                                 type="email"
                                 className="form-control"
                                 placeholder="Correo"
+                                name='registerEmail'
+                                value={registerEmail}
+                                onChange={onRegisterInputChange}
                             />
                         </div>
                         <div className="form-group mb-2">
@@ -53,6 +108,9 @@ export const LoginPage = () => {
                                 type="password"
                                 className="form-control"
                                 placeholder="Contraseña" 
+                                name='registerPassword'
+                                value={registerPassword}
+                                onChange={onRegisterInputChange}
                             />
                         </div>
 
@@ -61,6 +119,9 @@ export const LoginPage = () => {
                                 type="password"
                                 className="form-control"
                                 placeholder="Repita la contraseña" 
+                                name='registerPassword2'
+                                value={registerPassword2}
+                                onChange={onRegisterInputChange}
                             />
                         </div>
 
---
+++ 10-calendar/src/hooks/useAuthStore.js
@@ -0,0 +1,37 @@
+import { useDispatch, useSelector } from "react-redux"
+import {calendarApi } from "../api"
+import { onChecking, onLogin, onLogout, clearErrorMessage} from "../store/auth/authSlice";
+
+
+export const useAuthStore =()=>{
+
+    const { status, user, errorMessage} = useSelector (state => state.auth);
+    const dispatch = useDispatch();
+
+    const startLogin  =  async({email, password}) =>{
+        dispatch(onChecking());
+        try {
+           
+            const {data} = await calendarApi.post('/auth', {email, password});
+            localStorage.setItem('token', data.token);
+            localStorage.setItem('token-init-date', new Date().getTime());
+            dispatch(onLogin({name: data.name, uid: data.uid}));
+                       
+        } catch (error) {
+           dispatch(onLogout('Credenciales Incorrectas'));
+           setTimeout(() => {
+                dispatch (clearErrorMessage());
+           }, 10);
+        }
+    }
+
+    return {
+        //*propiedades
+        status, 
+        user, 
+        errorMessage,
+        startLogin
+
+        //* Metodosd
+    }
+}
